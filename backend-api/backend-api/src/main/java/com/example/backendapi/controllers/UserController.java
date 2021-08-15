6a76d7d3824eb8794aedc660b3a3e0c8c1d819b1
ronkiejay <79155451+ronkiejay@users.noreply.github.com>
2021-08-14 21:29:20 -0400

    Update UserController.java

--- backend-api/backend-api/src/main/java/com/example/backendapi/controllers/UserController.java
+++ backend-api/backend-api/src/main/java/com/example/backendapi/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Collections;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class UserController
 {
